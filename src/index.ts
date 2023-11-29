21a205a8154fb7860dacc6f812e437c694affeb1
Miika Anundi <manundi@gmail.com>
2023-11-29 11:02:50 +0200

    added grapsh wheen and now project running on dev

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import * as PIXI from 'pixi.js'
-import { Text, Container } from 'pixi.js'
+
+const screenWidth = window.innerWidth
+const screenHeight = window.innerHeight
+
 
 // define basic settings of html canvas. Sizer bg color etc.
 const game = new PIXI.Application<any>({
-    width: 800,
-    height: 600,
-    backgroundColor: 'white',
+    width: screenWidth,
+    height: screenHeight,
+    backgroundColor: 'black',
 });
 
 document.body.appendChild(game.view)
 
+const wheel : PIXI.Sprite = PIXI.Assets.load('/wheel.svg').then((resource) => {
+    return new PIXI.Sprite(resource)
+    wheel.setTransform(screenWidth / 2 - 50, 50, 0.3, 0.3)
+    game.stage.addChild(wheel);
+
+})
+
 const GameLoop = () => {
 console.log('frame drawn')
+
 
 }
 
